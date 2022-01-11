47a5ad419f79c4f8b20eed907ea487c93f08329f
Eghizio <eghizio@gmail.com>
2022-01-11 20:08:30 +0100

    Joke by ID, random

--- src/routes/jokes.ts
+++ src/routes/jokes.ts
@@ -7,3 +7,19 @@ export const jokes = Router();
 jokes.get("/", async (req, res) => {
     return res.json(jokesCollection);
 });
+
+jokes.get("/random", async (req, res) => {
+    const randomJoke = jokesCollection[Math.floor(Math.random() * jokesCollection.length)];
+    
+    return res.json(randomJoke);
+});
+
+jokes.get("/:id", async (req, res) => {
+    const requestedJoke = jokesCollection.find(joke => joke.id.toString() === req.params.id);
+
+    if(requestedJoke !== undefined){
+        return res.json(requestedJoke);
+    }
+
+    return res.sendStatus(404);
+});
